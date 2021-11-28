8258b79e74a1b5e268d4392abdd42752b70f78d1
jamesw <36804142+JamersonWalderson@users.noreply.github.com>
2021-11-28 16:56:36 -0300

    state text e onchange

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import React, { useState } from 'react';
 import './App.css';
 
 import SearchInput from './SearchInput';
 
 export default function App() {
+  const [text, setText] = useState('');
+
   return (
     <div className="App">
       <h1>Animes</h1>
-      <SearchInput />
+      <SearchInput value={text} onChange={(search) => setText(search)} />
     </div>
   );
 }
