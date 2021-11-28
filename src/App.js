2b64358072e588a16a9d5078f60479b9cd2bfe78
jamesw <36804142+JamersonWalderson@users.noreply.github.com>
2021-11-28 17:32:04 -0300

    chamada a api construida

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-
 import SearchInput from './SearchInput';
+
+const api = 'https://kitsu.io/api/edge/';
 
 export default function App() {
   const [text, setText] = useState('');
+
+  useEffect(() => {
+    if (text) {
+      fetch(`${api}anime?filter[text]=${text}`)
+        .then((response) => response.json())
+        .then((response) => console.log(response));
+    }
+
+  }, [text]);
 
   return (
     <div className="App">
--- src/SearchInput.js
+++ src/SearchInput.js
@@ -1,8 +1,15 @@
 import React from "react";
 
 const SearchInput = ({value, onChange}) => {
+    function handleChange(event) {
+        onChange(event.target.value);
+    }
     return (
-        <input type="search" value={value} onChange={onChange} />
+        <input 
+            type="search"
+            value={value}
+            onChange={handleChange}
+        />
     )
 };
 
