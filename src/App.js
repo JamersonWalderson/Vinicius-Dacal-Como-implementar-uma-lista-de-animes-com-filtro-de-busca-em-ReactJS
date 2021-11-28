e891f33a4a3591708c0ce518e528cfdb734a8d24
jamesw <36804142+JamersonWalderson@users.noreply.github.com>
2021-11-28 16:53:05 -0300

    criando componente SearchingInput

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 
+import SearchInput from './SearchInput';
+
 export default function App() {
   return (
     <div className="App">
       <h1>Animes</h1>
+      <SearchInput />
     </div>
   );
 }
