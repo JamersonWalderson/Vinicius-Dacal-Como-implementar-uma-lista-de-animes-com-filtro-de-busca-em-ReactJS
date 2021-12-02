0b569281d228b0293c9f7104f444b580f87224fa
jamesw <36804142+JamersonWalderson@users.noreply.github.com>
2021-12-02 11:57:40 -0300

    implementando debounce

--- src/SearchInput.js
+++ src/SearchInput.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import useDebounce from "./useDebounce";
 
 const SearchInput = ({ value, onChange }) => {
+  const [displayValue, setDisplayValue] = useState(value);
+  const debouncedChange = useDebounce(onChange, 500);
+
   function handleChange(event) {
-    onChange(event.target.value);
+    setDisplayValue(event.target.value);
+    debouncedChange(event.target.value);
+    // onChange(event.target.value);
   }
   return (
     <div className="row d-flex justify-content-center">
       <div className="col-md-8">
         <input
           type="search"
-          value={value}
+          value={displayValue}
           onChange={handleChange}
           className="form-control p-3 m-5"
           placeholder="Digite o nome do anime que será pesquisado"
