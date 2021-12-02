aa8f3b26378dc5fd434d4993f9e5cb36c78d3c1d
jamesw <36804142+JamersonWalderson@users.noreply.github.com>
2021-12-02 10:28:45 -0300

    testando codesandbox

--- src/SearchInput.js
+++ src/SearchInput.js
@@ -1,22 +1,22 @@
 import React from "react";
 
-const SearchInput = ({value, onChange}) => {
-    function handleChange(event) {
-        onChange(event.target.value);
-    }
-    return (
-        <div className="row">
-            <div className="col-md-6 d-flex justify-content-center">
-                <input 
-                    type="search"
-                    value={value}
-                    onChange={handleChange}
-                    className="form-control p-3 m-5"
-                    placeholder="Digite o nome do anime a ser pesquisado"
-                />
-            </div>
-        </div>
-    )
+const SearchInput = ({ value, onChange }) => {
+  function handleChange(event) {
+    onChange(event.target.value);
+  }
+  return (
+    <div className="row">
+      <div className="col-md-6 d-flex justify-content-center">
+        <input
+          type="search"
+          value={value}
+          onChange={handleChange}
+          className="form-control p-3 m-5"
+          placeholder="DDigite o nome do anime a ser pesquisado"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default SearchInput;
